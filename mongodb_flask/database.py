26b42f1aa69d14c1f662c4c0b296cae034a7eb8b
SkandaPrasad-S <skandaprasad98@gmail.com>
2023-07-08 15:51:26 +0530

    Fixed a leak

--- mongodb_flask/database.py
+++ mongodb_flask/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
-
+import os
 # Set the value of MONGODB_URI to your Atlas connection string.
-MONGODB_URI = 'mongodb+srv://skanda:u9kOzbS3Exj47sdO@cluster0.fyxz25q.mongodb.net/'
+MONGODB_URI = os.getenv('MONGODB_URI')
 
 
 def initialize_database(app):
